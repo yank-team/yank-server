59199e8c4713eb660252d478843778803b453cec
Nick Wood <nickswift498@gmail.com>
2014-05-13 11:03:37 -0700

    added haversine implementation

--- yank_server/helpers.py
+++ yank_server/helpers.py
@@ -2,6 +2,7 @@
 yank_server.helpers
 various helper functions to make development easier
 """
+import math
 
 def screen_methods(request, methods=['GET']):
     """
@@ -34,3 +35,25 @@ def std_response(msg="", success=True, data=None):
         'msg'    : msg,
         'data'   : data
     }
+
+"""
+implementation of the haversine functions -- we're using this to determine the
+distance between two points on the globe. 
+
+This isn't as accurate as it can be due to the elliptical nature of the actual
+globe, but in this context we don't care at all.
+"""
+def haversin(theta):
+    return (1-math.cos(theta))/2
+
+def haversine(lats, lngs):
+    a = haversin(lats[1]-lats[0])
+    b = math.cos(lats[0])*math.cos(lats[1])
+    c = haversin(lngs[1]-lngs[0])
+    return a + b*c
+
+def distance_between_globe_coords(point_a, point_b):
+    earth_radius = 6378.1
+    lats = (point_a[0], point_b[0])
+    lngs = (point_b[1], point_b[1])
+    return 2*earth_radius*math.asin(math.sqrt(haversine(lats, lngs)))
