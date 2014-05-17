bba7e83e32a5e262c4d749ab4ef0870c4ae5928d
Nick Wood <nickswift498@gmail.com>
2014-05-17 14:45:23 -0700

    deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,10 @@
 #! /bin/sh
-sudo su
 
 # pull from github
+echo "pulling from repo"
 git pull origin master
 
 # deploy apache configuration
-cp ./config/default /etc/apache2/sites-available/
-service apache2 restart
-
-# return to user
-exit
+echo "update apache config"
+sudo cp ./config/default /etc/apache2/sites-available/
+sudo service apache2 restart
