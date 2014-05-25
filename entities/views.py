55b6b9c8504ce07d7758c418c94e2fd90d6ea3f8
Nick Wood <nickswift498@gmail.com>
2014-05-24 17:14:49 -0700

    fixing csrf decorator bug

--- entities/views.py
+++ entities/views.py
@@ -13,6 +13,9 @@ import math, json
 
 
 class EntityView(View):
+    """
+    Either list or post entities to the DB
+    """
 
     # Generate a list of entities
     @csrf_exempt
@@ -23,27 +26,69 @@ class EntityView(View):
         ])
         return HttpResponse(res)
 
+    @csrf_exempt
     def post(self, request):
 
         data = json.loads(request.read())
 
         try:
             user = YankUser.objects.get(api_key=data['apik'])
-
-            # create and save the object
-            entity = Entity.objects.create(
-                name=data['name'],
-                lat=data['lat'],
-                lng=data['lng']
-            )
-            entity.save()
-
-            res = std_response(success=true, data={'eid': entity.id})
-            return HttpResponse(res)
-
         except ObjectDoesNotExist:
             res = std_response(success=False, msg='apik not supplied')
             return HttpResponse(res, 403)
+
+        # create and save the object
+        entity = Entity.objects.create(
+            name=data['name'],
+            lat=data['lat'],
+            lng=data['lng']
+        )
+        entity.save()
+
+        res = std_response(success=true, data={'eid': entity.id})
+        return HttpResponse(res)
+
+class EntityNoteView(View):
+
+    @csrf_exempt
+    def get(self, request):
+        """
+        get a list of notes
+        """
+        data = json.loads(request.read())
+
+        # serialize data from DB and return it
+        res = std_response(success=True, data=[
+            {'id': x.id, 'owner': x.owner, 'content': x.content}
+            for x in EntityNote.objects.filter(target=entity)
+        ])
+        return HttpResponse(res)
+
+    @csrf_exempt
+    def post(self, request):
+        """
+        post a new note
+        """
+        try:
+            user = YankUser.objects.get(api_key=data['apik'])
+        except ObjectDoesNotExist:
+            res = std_response(success=False, msg='apik invalid')
+            return HttpResponse(res, 403)
+        # attempt to create note. Note: this should fail gracefully thanks to 
+        # Django's default behavior
+        try:
+            note = EntityNote.objects.create(
+                owner=user,
+                target=Entity.objects.get(id__exact=data['eid']),
+                content=data['note']
+                )
+            note.save()
+        except ObjectDoesNotExist:
+            res = std_response(success=False, msg='eid invalid')
+            return HttpResponse(res, status=404)
+
+        res = std_response(success=True, data={'nid': note.id})
+        return HttpResponse(res)
 
 @csrf_exempt
 def list_entities_inside_radius(request, radius='5'):
@@ -92,72 +137,3 @@ def list_entities_inside_radius(request, radius='5'):
         for x in entities
     ]
     return HttpResponse(json.dumps(ret))
-
-
-@csrf_exempt
-def list_entity_notes(request, eid='1'):
-    """
-    list notes tied to a given entity 
-    """
-    if not screen_methods(request):
-        return HttpResponse(status=405)
-
-    # find entity from eid 
-    try:
-        entity = Entity.objects.get(id__exact=int(eid))
-    except ObjectDoesNotExist:
-        return HttpResponse('entity does not exist', 404)
-
-    # list all notes associated with this entity
-    notes = EntityNote.objects.filter(target=entity)
-
-    ret = {
-        'entity' : entity.id,
-        'notes'  : [
-            {
-                'owner'   : x.id,
-                'content' : x.content
-            }
-            for x in notes
-        ]
-    }
-
-    return HttpResponse(json.dumps(ret))
-
-
-@csrf_exempt
-def post_entity_note(request):
-    """ 
-    create a new note from a given user for a given entity 
-    """
-    if not screen_methods(request, ['POST']):
-        return HttpResponse(status=405)
-
-    # create a new note 
-    data = json.loads(request.read())
-
-    if not screen_attrs(data, ['apik', 'eid', 'note']):
-        return HttpResponse('improperly formatted request', status=400)
-
-    # find user from APIK 
-    try:
-        user = YankUser.objects.get(api_key=data['apik'])
-    except ObjectDoesNotExist:
-        return HttpResponse('given API key is invalid', 403)
-
-    # find entity from eid 
-    try:
-        entity = Entity.objects.get(id__exact=data['eid'])
-    except ObjectDoesNotExist:
-        return HttpResponse('entity does not exist', 404)
-
-    # attempt to create note. Note: this should fail gracefully thanks to 
-    # Django's default behavior
-    note = EntityNote.objects.create(
-        owner=user,
-        target=entity,
-        content=data['note']
-        )
-    note.save()
-
-    return HttpResponse('success')
