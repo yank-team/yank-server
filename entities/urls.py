ba3b36d8018bf90ba21701566e476c03f8bb82b8
Nick Wood <nickswift498@gmail.com>
2014-05-24 20:02:59 -0700

    range durp

--- entities/urls.py
+++ entities/urls.py
@@ -12,6 +12,6 @@ urlpatterns = patterns('',
     url(r'^compound/$', EntityNoteCompoundPostView.as_view()),
 
     # Get entities within a radius
-    url(r'^radius/(?P<lat>[0-9]\.[0-9]+)/(?P<lng>[0-9]\.[0-9]+)/(?P<radius>[0-9]+)/$',
+    url(r'^radius/(?P<arg_lat>[0-9]\.[0-9]+)/(?P<arg_lng>[0-9]\.[0-9]+)/(?P<radius>[0-9]+)/$',
         'entities.views.list_entities_inside_radius'),
 )
